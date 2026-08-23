7bc2a9f4e238106593158cc9443e23371d0e812b
fan-siyang0622 <3433899103@qq.com>
2026-08-24 02:11:52 +0800

    添加项目文件。

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+﻿#include"vector.h"
+#include<iostream>
+
+int main()
+{
+	/*allocator<int> give;
+	int* a = give.allocate(10);
+	for (int i = 0; i < 10; i++)
+	{
+		give.construct(a+i,i);
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << a[i] << std::endl;
+	}
+	give.destory(a);
+	give.deallocate(a);*/
+
+	return 0;
+}
---
+++ vector.h
@@ -0,0 +1,92 @@
+﻿#pragma once
+#include<vector>
+#include<exception>
+
+template<class T>
+class vector
+{
+	using value_type = T;
+	using pointer = T*;
+	using const_pointer = const T*;
+	using reference = T&;
+	using const_reference = const T&;
+	using size_type = std::size_t;
+	using difference_type = std::ptrdiff_t;
+public:
+	vector()
+		:_start(nullptr)
+		, _end(nullptr)
+		, _end_of_storage(nullptr)
+	{
+	}
+private:
+	T* _start;
+	T* _end;
+	T* _end_of_storage;
+};
+
+template<class T>
+class allocator
+{
+	using value_type = T;
+	using pointer = T*;
+	using const_pointer = const T*;
+	using reference = T&;
+	using const_reference = const T&;
+	using size_type = std::size_t;
+	using difference_type = std::ptrdiff_t;
+
+public:
+	template<class U>
+	struct rebind
+	{
+		using other = allocator<U>;
+	};
+
+	allocator() noexcept {};
+
+	allocator(const allocator<value_type>& alloc) noexcept {};
+
+	template<class U>
+	allocator(const allocator<U>& alloc) noexcept {};
+
+	pointer allocate(size_type n)
+	{
+		return static_cast<pointer>(::operator new(n * sizeof(value_type)));
+	}
+
+	void deallocate(pointer p) noexcept
+	{
+		::operator delete(p);
+	}
+
+	size_type max_size() const noexcept
+	{
+		return static_cast<size_type>(-1) / sizeof(value_type);
+	}
+
+	template<class U,class... Args>
+	void construct(U* p, Args&&... args)
+	{
+		new(p) U(std::forward<Args>(args)...);
+	}
+
+	template<class U>
+	void destory(U* p) noexcept
+	{
+		p->~U();
+	}
+
+};
+
+template<class T, class U>
+bool operator==(const allocator<T>&, const allocator<U>&) noexcept
+{
+	return true;
+}
+
+template<class T, class U>
+bool operator!=(const allocator<T>&, const allocator<U>&) noexcept
+{
+	return false;
+}
